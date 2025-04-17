8de9fc153993a538cb1603c3f64451e66fb8e69e
AhnafThowfeek <138268265+AhnafThowfeek@users.noreply.github.com>
2025-04-17 21:28:46 +0530

    complete signup page UI

--- api/index.js
+++ api/index.js
@@ -24,3 +24,13 @@ mongoose.connect(MONGOURL).then(() =>{
 app.use(express.json());
 app.use('/api/user', userRoutr);
 app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
