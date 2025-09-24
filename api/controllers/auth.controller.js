1af071f00ab00f370d2a7dcebca1e63a34f8c8bd
AhnafThowfeek <138268265+AhnafThowfeek@users.noreply.github.com>
2025-09-24 13:32:07 +0530

    create profile

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -36,6 +36,7 @@ export const google = async (req, res, next) =>{
         // Fixed: Changed res.body to req.body
         const user = await User.findOne({email: req.body.email});
         if(user){
+            
             const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
             const {password: pass, ...rest} = user._doc;
             res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
